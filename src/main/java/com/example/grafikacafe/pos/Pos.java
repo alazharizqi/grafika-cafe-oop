7a3b56c88f0ac26c55f97c440f35d335672e6033
unknown <alazharisqi@gmail.com>
2022-05-27 18:31:18 +0700

    'edit'

--- src/main/java/com/example/grafikacafe/pos/Pos.java
+++ src/main/java/com/example/grafikacafe/pos/Pos.java
@@ -1,12 +1,50 @@
 package com.example.grafikacafe.pos;
 
 import com.example.grafikacafe.Main;
+import com.example.grafikacafe.connection.SqiliteConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
 
-public class Pos {
+public class Pos implements Initializable {
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    @FXML
+    private ComboBox menu = new ComboBox();
+
+    ObservableList<String> kode_menuItems = FXCollections.observableArrayList();
+
+    public void setKode_menuItems() {
+        String query = "select * from tb_menu where status = ?";
+        connection = SqiliteConnection.Connector();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "Available");
+            resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                kode_menuItems.add(resultSet.getString("menu"));
+            }
+            preparedStatement.close();
+            resultSet.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        menu.setItems(kode_menuItems);
+    }
+
 
     @FXML
     public void logout (ActionEvent event) throws IOException {
@@ -20,4 +58,8 @@ public class Pos {
         change.changeScene("pos/Sales.fxml");
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setKode_menuItems();
+    }
 }
